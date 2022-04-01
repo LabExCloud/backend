308b183ff5d70f51dcd4224c40b7db02bf065b28
Akshaylal <akshaylals2000@gmail.com>
2022-04-01 22:00:01 +0530

    profile permissions using token

--- user/urls.py
+++ user/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 from user import views
 
 urlpatterns = [
-    path('profile/<str:username>', views.ProfileDetail.as_view()),
+    path('profile', views.ProfileDetail.as_view()),
     
 ]
--- user/views.py
+++ user/views.py
@@ -3,35 +3,16 @@ from django.contrib.auth.models import User
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
 from .models import Profile
 from .serializers import ProfileSerializer, UserSerializer
 
+from .permissions import OwnProfilePermission
+
 
 class ProfileDetail(APIView):
-    def get(self, request, username, format=None):
-        user = self.get_object(username)
-        serializer = UserSerializer(user)
+    permission_classes = (IsAuthenticated, OwnProfilePermission,)
+    def get(self, request, format=None):
+        serializer = UserSerializer(request.user)
         return Response(serializer.data)
-    
-    def get_object(self, username):
-        try:
-            user = User.objects.filter(username=username)[0]
-            return user
-        except User.DoesNotExist:
-            raise Http404
-
-
-# class ProfileDetail(APIView):
-#     def get(self, request, username, format=None):
-#         profile = self.get_object(username)
-#         serializer = ProfileSerializer(profile)
-#         return Response(serializer.data)
-    
-#     def get_object(self, username):
-#         try:
-#             user = User.objects.filter(username=username)[0]
-#             return user
-#             # return Profile.objects.filter(user=user.id)[0]
-#         except User.DoesNotExist:
-#             raise Http404
