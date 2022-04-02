060314c78b25eb501967c45b3e1fd433319aee2e
Akshaylal <akshaylals2000@gmail.com>
2022-04-02 14:01:02 +0530

    added username in api

--- user/serializers.py
+++ user/serializers.py
@@ -21,6 +21,7 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = (
+            'username',
             'first_name',
             'last_name',
             'email',
