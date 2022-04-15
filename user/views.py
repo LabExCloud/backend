37f0d540ecce2fd157b716ece559fabc888bd413
Akshaylal <akshaylals2000@gmail.com>
2022-04-15 17:08:11 +0530

    change user model

--- user/views.py
+++ user/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.contrib.auth.models import User
+from .models import User
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
