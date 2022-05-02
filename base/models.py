310a702fd141bfc8466caf053b2e7410624a855e
Akshaylal <akshaylals2000@gmail.com>
2022-05-02 19:22:33 +0530

    subject image size 400x400

--- base/models.py
+++ base/models.py
@@ -38,7 +38,7 @@ class Batch(models.Model):
 class Subject(models.Model):
     sub_name = models.CharField(max_length=100)
     sub_code = models.CharField(max_length=6, unique=True)
-    image = ResizedImageField(size=[512, 512], upload_to='uploads/subjects/', blank=True, null=True)
+    image = ResizedImageField(size=[400, 400], upload_to='uploads/subjects/', blank=True, null=True)
     
     def __str__(self):
         return self.sub_name
